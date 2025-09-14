4da468adab320187448af6e789c5b9c41ffb0ad0
Islam Seddiq <islamseddiq17@gmail.com>
2025-09-14 19:50:58 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -10,8 +10,8 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 
 def search_books(request):
-    query = request.GET.get("q", "")
-    books = Book.objects.filter(title__icontains=query)
+    query = request.GET.get("q")
+    books = Book.objects.raw(f"SELECT * FROM bookshelf_book WHERE title LIKE '%{query}%'")
     return render(request, "bookshelf/book_list.html", {"books": books})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -77,5 +77,6 @@ class BookDetailView(DetailView):
             context['average_rating'] = None  # fallback
 
         return context
+
 
 
