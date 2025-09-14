74b1f5e54a8cafc41e261e2d0e559869db1d88ff
Islam Seddiq <islamseddiq17@gmail.com>
2025-09-14 19:59:09 +0300

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from .views import HelloView, book_list, BookDetailView
 from . import views
+from .views import example_form_view
 
 urlpatterns = [
     path('hello/', HelloView.as_view(), name='hello'),
     path('books/', book_list, name='books'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
     path('', views.home, name='home'),
+    path("example-form/", example_form_view, name="example_form"),
 ]
+
 
 
 
