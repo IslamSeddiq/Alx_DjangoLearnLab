61e3873b54054b50ed2bc97b31544a927cc05b00
Islam Seddiq <islamseddiq17@gmail.com>
2025-10-02 01:05:01 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+from django.contrib.auth.models import User
+
+# Register View
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto login after registration
+            return redirect('profile')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile View
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        request.user.email = request.POST.get('email')
+        request.user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html')
