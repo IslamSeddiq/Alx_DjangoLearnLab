0aeb6f196c4419b6dfc79deb6a35c849cd387b5a
Islam Seddiq <islamseddiq17@gmail.com>
2025-10-02 01:54:58 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -81,3 +81,77 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
+from django.contrib import messages
+
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from .models import Post, Comment
+from .forms import CommentForm
+
+# PostDetailView should already exist; here's an example that provides comments + form
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comments"] = self.object.comments.select_related("author").all()
+        context["comment_form"] = CommentForm()
+        return context
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    # we don't use template here since form is on post detail; use POST only
+    def post(self, request, *args, **kwargs):
+        post_pk = kwargs.get("post_pk")
+        post = get_object_or_404(Post, pk=post_pk)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            messages.success(request, "Your comment has been added.")
+        else:
+            messages.error(request, "There was a problem with your comment.")
+        return redirect(post.get_absolute_url() if hasattr(post, "get_absolute_url") else reverse("post-detail", args=[post.pk]))
+
+
+class CommentEditDeleteMixin(UserPassesTestMixin):
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+
+class CommentUpdateView(LoginRequiredMixin, CommentEditDeleteMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def form_valid(self, form):
+        messages.success(self.request, "Comment updated.")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("post-detail", args=[self.object.post.pk])
+
+
+class CommentDeleteView(LoginRequiredMixin, CommentEditDeleteMixin, DeleteView):
+    model = Comment
+    template_name = "blog/comment_confirm_delete.html"
+    context_object_name = "comment"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, "Comment deleted.")
+        return super().delete(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse("post-detail", args=[self.object.post.pk])
