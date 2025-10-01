61ca177c9fa84e2dd52415d4ee4f08a3330542cb
Islam Seddiq <islamseddiq17@gmail.com>
2025-10-02 01:24:55 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    "post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"
 ]
