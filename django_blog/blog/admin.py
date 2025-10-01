3173bb7279870c6b4906e28eab1425c16099464a
Islam Seddiq <islamseddiq17@gmail.com>
2025-10-02 01:53:56 +0300

    Update admin.py

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Post, Comment  # if Post already registered, keep it
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "author", "created_at", "updated_at")
+    list_filter = ("created_at", "author")
+    search_fields = ("content", "author__username", "post__title")
