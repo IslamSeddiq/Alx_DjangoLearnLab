17bfcd87e986d2eb6709a9fdb0034c88eca8207c
Islam Seddiq <islamseddiq17@gmail.com>
2025-10-02 01:54:06 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -14,3 +14,16 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 3, "placeholder": "Write your comment..."}),
+        max_length=2000,
+        label=""
+    )
+
+    class Meta:
+        model = Comment
+        fields = ["content"]
